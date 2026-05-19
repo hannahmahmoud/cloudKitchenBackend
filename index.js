e7fe66f8122be40908b2880eeba61620dea06924
hannahmahmoud <hanamahmoud178@gmail.com>
2026-05-19 11:13:16 +0300

    adjusted server.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const mongoose = require('mongoose');
+const dotenv   = require('dotenv');
+const dns      = require('dns');
+
+dotenv.config({ path: 'config.env' });
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 const app = require('./App/app');
+
+mongoose.connect(process.env.CONN_STR).catch((err) => {
+  console.log('MongoDB connection error: ' + err);
+});
 
 module.exports = app;
